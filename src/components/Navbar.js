544b0be3cd9ecb24a96df73a9aa3e49008398c3d
Arman_Shanto <hossain.arman@gmail.com>
2021-05-26 15:53:27 +0600

    my resume deployment

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+    AppBar,
+    Toolbar,
+    ListItem,
+    IconButton,
+    ListItemText,
+    Avatar,
+    Divider,
+    List,
+    Typography,
+    Box
+} from '@material-ui/core';
+
+import {
+    ArrowBack,
+    AssignmentInd,
+    Home,
+    Apps,
+    ContactMail
+} from '@material-ui/icons'
+
+const Navbar = () => {
+    return (
+        <Box component="nav">
+            <AppBar position="static" style={{background:'#222'}}>
+                <Toolbar>
+                    <IconButton>
+                        <ArrowBack style={{background:'tomato'}}/>
+                    </IconButton>
+                    <Typography variant="h5" style={{color:'tan'}}>
+                        Portfolio
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+};
+
+export default Navbar;
